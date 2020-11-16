f27310675f87e4a029f0d7b99cf78b2c6a01baf3
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-16 04:50:54 +0000

    CREATE : organizings_controller

---
+++ app/controllers/organizings_controller.rb
@@ -0,0 +1,2 @@
+class OrganizingsController < ApplicationController
+end
---
+++ test/controllers/organizings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OrganizingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

a143f588c7f305dd953873e4189a27b4c94b7384
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-17 07:18:01 +0000

    UPDATE : groups_show members list

---
+++ app/helpers/groups_helper.rb
@@ -0,0 +1,16 @@
+module GroupsHelper
+  def chk_member_status(user_id)
+    case user_id
+    when @group.owner.id
+      "オーナー"
+    when *@organizers.pluck(:user_id)
+      "オーガナイザー"
+    when *@pending_users.pluck(:user_id)
+      "保留中"
+    when *@members.pluck(:user_id)
+      "メンバー"
+    else
+      "未加入"
+    end
+  end
+end

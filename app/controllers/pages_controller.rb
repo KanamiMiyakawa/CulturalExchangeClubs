5b6b006e3ea132f97d53cfb5bc616140697fe443
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-18 06:31:56 +0000

    UPDATE : user_show to display user's_group list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+
+  #開発時の暫定トップ
   def devtop
   end
+
   def profile
     @user = User.find(params[:id])
+    @members = @user.members.includes(:group) if @user == current_user
   end
 end

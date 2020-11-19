3b0a277e0761f3610085bdf3111ce128d03178c1
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-19 02:49:42 +0000

    g event model

---
+++ db/migrate/20201119024209_create_events.rb
@@ -0,0 +1,22 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name, null: false
+      t.datetime :schedule, null: false
+      t.references :organizer, foreign_key: true
+      t.references :group, foreign_key: true
+      t.text :content, null: false
+      t.boolean :online, default: false
+      t.boolean :permission, default: false
+      t.boolean :guest_allowed, default: false
+      t.string :address, default: ""
+      t.float :lat
+      t.float :lon
+      t.string :place, default: ""
+
+      t.timestamps
+    end
+
+    add_index :events, [:name, :schedule]
+  end
+end

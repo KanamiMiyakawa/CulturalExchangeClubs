7ac16e5695abbc3f968986ac2ee50d7f764c7b42
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-19 04:01:23 +0000

    UPDATE : event_model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  geocoded_by :address, latitude: :lat, longitude: :lon
+  after_validation :geocode, if: :address_changed?
+  
   belongs_to :organizer
   belongs_to :group
 end

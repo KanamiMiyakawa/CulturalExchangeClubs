fee56472e4dfe7d5ea8051845b1e4592f3db2338
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-27 08:32:46 +0000

    UPDATE : participant_controller create action

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,25 @@
 class ParticipantsController < ApplicationController
   def create
-    @participant = current_user.participants.build(participant_params)
-    if @participant.save
-      redirect_to event_path(params[:event_id]), notice: "イベントに参加リクエストを送りました！\n現在のステータスをご確認ください"
+    if params[:guest] == "false" && !Event.find(params[:event_id]).group.users.include?(current_user)
+      @participant = current_user.participants.build(participant_params)
+      if @participant.save
+        group = Event.find(params[:event_id]).group
+        if group.permission
+          current_user.members.create!(group_id:group.id, pending: true)
+        else
+          current_user.members.create!(group_id:group.id)
+        end
+        redirect_to event_path(params[:event_id]), notice: "イベントとグループに参加リクエストを送りました！\n現在のステータスをご確認ください"
+      else
+        redirect_to event_path(params[:event_id]), notice: "参加リクエストが送れませんでした"
+      end
     else
-      redirect_to event_path(params[:event_id]), notice: "参加リクエストが送れませんでした"
+      @participant = current_user.participants.build(participant_params)
+      if @participant.save
+        redirect_to event_path(params[:event_id]), notice: "イベントに参加リクエストを送りました！\n現在のステータスをご確認ください"
+      else
+        redirect_to event_path(params[:event_id]), notice: "参加リクエストが送れませんでした"
+      end
     end
   end
 

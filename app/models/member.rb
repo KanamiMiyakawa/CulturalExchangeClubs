e505b3cdb609498853110ee804034838ce998834
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-28 07:35:32 +0000

    UPDATE : member_model add callback

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,8 @@
 class Member < ApplicationRecord
   before_create :check_pending
+  before_destroy :deny_event_request
+  before_destroy :change_member_to_guest
+
   belongs_to :user
   belongs_to :group
 
@@ -9,6 +12,18 @@ class Member < ApplicationRecord
 
   def check_pending
     self.pending = true if self.group.permission?
+  end
+
+  #メンバーを削除するとき、ゲスト不可のイベント参加を削除する
+  def deny_event_request
+    events = self.group.events.where('schedule >= ?', Time.zone.now).where(guest_allowed:false)
+    Participant.where(user_id:self.user_id, event_id:events.pluck(:id)).destroy_all if events.present?
+  end
+
+  #メンバーを削除するとき、ゲスト可のイベント参加があれば、その元メンバーをゲストにする
+  def change_member_to_guest
+    events = self.group.events.where('schedule >= ?', Time.zone.now).where(guest_allowed:true)
+    Participant.where(user_id:self.user_id, event_id:events.pluck(:id)).update_all(guest:true) if events.present?
   end
 
   # def owner_not_destroyed

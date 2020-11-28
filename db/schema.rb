dd082cc826bd627f6faedb3a78bc24d7da61d19e
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-28 09:32:26 +0000

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_120806) do
+ActiveRecord::Schema.define(version: 2020_11_28_093100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,9 @@ ActiveRecord::Schema.define(version: 2020_11_27_120806) do
     t.boolean "guest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id"
     t.index ["event_id"], name: "index_participants_on_event_id"
+    t.index ["group_id"], name: "index_participants_on_group_id"
     t.index ["language_id"], name: "index_participants_on_language_id"
     t.index ["user_id"], name: "index_participants_on_user_id"
   end
@@ -132,6 +134,7 @@ ActiveRecord::Schema.define(version: 2020_11_27_120806) do
   add_foreign_key "organizers", "groups"
   add_foreign_key "organizers", "users"
   add_foreign_key "participants", "events"
+  add_foreign_key "participants", "groups"
   add_foreign_key "participants", "languages"
   add_foreign_key "participants", "users"
 end

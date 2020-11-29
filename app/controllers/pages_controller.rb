487e6fbaf46f479f9dcc835cb653daf6bc8c0021
kanami miyakawa <kedamap@yahoo.co.jp>
2020-11-29 08:38:51 +0000

    UPDATE : profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,7 @@ class PagesController < ApplicationController
   def profile
     @user = User.find(params[:id])
     @members = @user.members.includes(:group) if @user == current_user
+    @events = @user.events.where('schedule >= ?', Time.zone.now).order(schedule: "ASC")
+    @index_date = 0
   end
 end

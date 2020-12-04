cf2362542df0b695fbf2e598ca69b753fa3e5638
kanami miyakawa <kedamap@yahoo.co.jp>
2020-12-04 14:34:18 +0000

    UPDATE : events_index page view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,11 @@ class EventsController < ApplicationController
   def index
     @events = Event.where('schedule >= ?', Time.zone.now).order(schedule: "ASC").limit(20)
     @index_date = 0
+    if user_signed_in?
+      @user = current_user
+      @coming_events = @user.events.where('schedule >= ?', Time.zone.now).order(schedule: "ASC").limit(3)
+      @organizing_events = @user.organizing_events.where('schedule >= ?', Time.zone.now).order(schedule: "ASC").limit(3)
+    end
   end
 
   def show

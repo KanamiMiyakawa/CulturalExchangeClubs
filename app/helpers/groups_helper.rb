fbff29cd352d72554b1bc704f61e5dff71ef33bb
kanami miyakawa <kedamap@yahoo.co.jp>
2020-12-06 08:42:12 +0000

    UPDATE : group_edit page view

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,3 +1,19 @@
 module GroupsHelper
+
+  def group_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      groups_path
+    elsif action_name == 'edit' || action_name == 'update'
+      organizing_group_path
+    end
+  end
+
+  def group_submit_btn
+    if action_name == 'new' || action_name == 'create'
+      t('helpers.btn.create_group')
+    elsif action_name == "edit"
+      t('helpers.btn.edit_group')
+    end
+  end
   
 end

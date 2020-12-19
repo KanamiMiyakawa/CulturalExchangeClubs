7f531828530e5af9264597b9b444702027290c02
kanami miyakawa <kedamap@yahoo.co.jp>
2020-12-19 16:09:00 +0000

    UPDATE : group_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,18 @@
 FactoryBot.define do
 
   factory :test_user_1, class: User do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     name { 'test_user_1' }
     email { 'test_user_1@example.com' }
     introduction { 'Hello!' }
     address { '東京都墨田区押上１丁目１−２' }
     password { 'test_user_1@example.com' }
+  end
+
+  factory :test_user_2, class: User do
+    name { 'test_user_2' }
+    email { 'test_user_2@example.com' }
+    introduction { 'Hello!' }
+    address { '東京都港区芝公園４丁目２−８' }
+    password { 'test_user_2@example.com' }
   end
 end
--- spec/system/group_spec.rb
+++ spec/system/group_spec.rb
@@ -39,4 +39,61 @@ describe 'グループCRUD機能', type: :system do
       end
     end
   end
+  describe 'グループ削除機能' do
+    context 'ログイン後、グループを削除した場合' do
+      before do
+        @user_1 = FactoryBot.create(:test_user_1)
+        @group_1 = @user_1.own_groups.create!(name: "test_group_1", detail: "This is test_group_1")
+      end
+      it 'グループ削除noticeが表示' do
+        visit new_user_session_path
+        fill_in "user[email]", with: 'test_user_1@example.com'
+        fill_in "user[password]", with: 'test_user_1@example.com'
+        click_on 'commit'
+
+        visit organizing_path
+        page.accept_confirm do
+          click_on "group#{@group_1.id}-delete-btn"
+        end
+        expect(page).to have_content 'グループを削除しました'
+      end
+    end
+  end
+  describe 'グループ参加機能' do
+    context '自由参加のグループに参加した場合' do
+      before do
+        @user_1 = FactoryBot.create(:test_user_1)
+        @user_2 = FactoryBot.create(:test_user_2)
+        @group_1 = @user_1.own_groups.create!(name: "test_group_1", detail: "This is test_group_1", permission: false)
+      end
+      it 'グループメンバーが2人になっている' do
+        visit new_user_session_path
+        fill_in "user[email]", with: 'test_user_2@example.com'
+        fill_in "user[password]", with: 'test_user_2@example.com'
+        click_on 'commit'
+
+        visit group_path(@group_1.id)
+        click_on 'group-request-btn'
+        expect(@group_1.members.count).to eq 2
+      end
+    end
+    context '承認制のグループに参加し、オーガナイザーが許可した場合' do
+      before do
+        @user_1 = FactoryBot.create(:test_user_1)
+        @user_2 = FactoryBot.create(:test_user_2)
+        @group_1 = @user_1.own_groups.create!(name: "test_group_1", detail: "This is test_group_1", permission: true)
+        @membership_1 = Member.create!(user_id: @user_2.id, group_id: @group_1.id, pending: true)
+      end
+      it 'リクエスト承認noticeが表示' do
+        visit new_user_session_path
+        fill_in "user[email]", with: 'test_user_1@example.com'
+        fill_in "user[password]", with: 'test_user_1@example.com'
+        click_on 'commit'
+
+        visit organizing_path
+        click_on "accept-member#{@membership_1.id}"
+        expect(page).to have_content 'リクエストを許可しました'
+      end
+    end
+  end
 end

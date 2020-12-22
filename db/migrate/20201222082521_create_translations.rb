f2136a7cbbc83741c1297df8d9e3bdfe366e8ada
kanami miyakawa <kedamap@yahoo.co.jp>
2020-12-22 08:26:11 +0000

    generate model translation

---
+++ db/migrate/20201222082521_create_translations.rb
@@ -0,0 +1,11 @@
+class CreateTranslations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :translations do |t|
+      t.references :events, foreign_key: true
+      t.text :content
+      t.string :code
+
+      t.timestamps
+    end
+  end
+end

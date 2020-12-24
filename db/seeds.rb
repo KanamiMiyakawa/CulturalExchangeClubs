752816beb74b145cee911146ae35835ca66f5ff7
kanami miyakawa <kedamap@yahoo.co.jp>
2020-12-24 02:03:30 +0000

    UPDATE : seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,26 @@ CSV.foreach('db/csv/iso693-1-language-codes-ja-sorted.csv', headers: true) do |r
   )
 end
 
-CSV.foreach('db/csv/seed_data_user.csv', headers: true) do |row|
-  User.create!(
-    name: row['name'],
-    email: row['name'],
-    password: row['name'],
-    address: row['address'],
-    introduction: "Hello! This is default introduction"
-  )
+# devではgoogleMapsが使えないことを想定
+if Rails.env == "development"
+  CSV.foreach('db/csv/seed_data_user.csv', headers: true) do |row|
+    User.create!(
+      name: row['name'],
+      email: row['name'],
+      password: row['name'],
+      introduction: "Hello! This is default introduction"
+    )
+  end
+else
+  CSV.foreach('db/csv/seed_data_user.csv', headers: true) do |row|
+    User.create!(
+      name: row['name'],
+      email: row['name'],
+      password: row['name'],
+      address: row['address'],
+      introduction: "Hello! This is default introduction"
+    )
+  end
 end
 
 # user1~5はそれぞれgroup1~5のオーナー
@@ -59,36 +71,6 @@ end
   end
 end
 
-
-# リアルイベント30件
-# イベント個数
-s = 0
-
-# 6種類を5回ずつ作成
-event_n = []
-6.times do |n|
-  5.times do |m|
-    event_n.push(n)
-  end
-end
-event_n.shuffle!
-
-name_array = ["LanguageExchangeお茶会しよう！",
-              "お花見に行こう！",
-              "カフェでおしゃべり",
-              "フットサル大会！",
-              "BBQパーティ！",
-              "ボードゲームナイト！"
-            ]
-
-content_array = ["LanguageExchangeはお互いの言語を学びたい人同士が集まり、おしゃべりする場です！",
-                  "この近くには桜のきれいな場所がたくさんあります！\n気候もよくなってきたのでみんなで見に行きましょう！",
-                  "おいしいコーヒーはお好きですか？\nお洒落なカフェでみんなでおしゃべりしましょう",
-                  "運動不足のメンバーが多いと聞いてこのイベントを企画しました\n近くのフットサル場で遊びましょう！",
-                  "BBQしながらみんなで美味しいお肉を食べましょう！\n材料は持ちよりなので、余っている料理や食材などがあったら是非持ってきてください",
-                  "楽しくゲームをプレイしながら、お互いに交流しましょう！\n色々なおもしろいゲームがあります！"
-                ]
-
 # 言語は日本語と何でも、中国語、韓国語、ベトナム語、英語、ポルトガル語のいずれか
 language_ids = [1,140,148,176,177,182]
 
@@ -97,64 +79,96 @@ def time_rand from = Time.zone.now, to = Time.zone.now + 30.days
   Time.zone.at((from + rand * (to.to_f - from.to_f)).to_i / 60 * 60)
 end
 
-CSV.foreach('db/csv/seed_data_address.csv', headers: true) do |row|
-  name = name_array[event_n[s]]
-  content = content_array[event_n[s]]
+# developmentではgoogleMapsが使えないことを想定
+if !Rails.env == "development"
+  # リアルイベント30件
+  # イベント個数
+  s = 0
 
-  permission = rand(2)
-  if permission == 0
-    permission = true
-  else
-    permission = false
+  # 6種類を5回ずつ作成
+  event_n = []
+  6.times do |n|
+    5.times do |m|
+      event_n.push(n)
+    end
   end
+  event_n.shuffle!
 
-  guest = rand(2)
-  if guest == 0
-    guest = true
-  else
-    guest = false
+  name_array = ["LanguageExchangeお茶会しよう！",
+                "お花見に行こう！",
+                "カフェでおしゃべり",
+                "フットサル大会！",
+                "BBQパーティ！",
+                "ボードゲームナイト！"
+              ]
+
+  content_array = ["LanguageExchangeはお互いの言語を学びたい人同士が集まり、おしゃべりする場です！",
+                    "この近くには桜のきれいな場所がたくさんあります！\n気候もよくなってきたのでみんなで見に行きましょう！",
+                    "おいしいコーヒーはお好きですか？\nお洒落なカフェでみんなでおしゃべりしましょう",
+                    "運動不足のメンバーが多いと聞いてこのイベントを企画しました\n近くのフットサル場で遊びましょう！",
+                    "BBQしながらみんなで美味しいお肉を食べましょう！\n材料は持ちよりなので、余っている料理や食材などがあったら是非持ってきてください",
+                    "楽しくゲームをプレイしながら、お互いに交流しましょう！\n色々なおもしろいゲームがあります！"
+                  ]
+
+  CSV.foreach('db/csv/seed_data_address.csv', headers: true) do |row|
+    name = name_array[event_n[s]]
+    content = content_array[event_n[s]]
+
+    permission = rand(2)
+    if permission == 0
+      permission = true
+    else
+      permission = false
+    end
+
+    guest = rand(2)
+    if guest == 0
+      guest = true
+    else
+      guest = false
+    end
+
+    language_ids.shuffle!
+    max = rand(10..20)
+
+    event = Event.create!(
+      name: name,
+      schedule: time_rand,
+      organizer_id: s%5 + 1,
+      group_id: s%5 + 1,
+      user_id: s%5 + 1,
+      content: content,
+      address: row['address'],
+      permission: permission,
+      guest_allowed: guest,
+      event_languages_attributes: [
+        {
+          language_id: 184,
+          max: max
+        },
+        {
+          language_id: language_ids[0],
+          max: max
+        }
+      ]
+    )
+
+    case event_n[s]
+    when 0
+      event.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_conversation.mp4")), filename: "seed_conversation.mp4", content_type: 'video/mp4')
+    when 1
+      event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_flower.jpg")), filename: "seed_flower.jpg", content_type: 'image/jpg')
+    when 2
+      event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_coffee.jpg")), filename: "seed_coffee.jpg", content_type: 'image/jpg')
+    when 4
+      event.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_bbq.mp4")), filename: "seed_bbq.mp4", content_type: 'video/mp4')
+      event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_bbq_thombnail.jpg")), filename: "seed_bbq_thombnail.jpg", content_type: 'image/jpg')
+    when 5
+      event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_game.jpg")), filename: "seed_game.jpg", content_type: 'image/jpg')
+    end
+
+    s += 1
   end
-
-  language_ids.shuffle!
-  max = rand(10..20)
-
-  event = Event.create!(
-    name: name,
-    schedule: time_rand,
-    organizer_id: s%5 + 1,
-    group_id: s%5 + 1,
-    user_id: s%5 + 1,
-    content: content,
-    address: row['address'],
-    permission: permission,
-    guest_allowed: guest,
-    event_languages_attributes: [
-      {
-        language_id: 184,
-        max: max
-      },
-      {
-        language_id: language_ids[0],
-        max: max
-      }
-    ]
-  )
-
-  case event_n[s]
-  when 0
-    event.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_conversation.mp4")), filename: "seed_conversation.mp4", content_type: 'video/mp4')
-  when 1
-    event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_flower.jpg")), filename: "seed_flower.jpg", content_type: 'image/jpg')
-  when 2
-    event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_coffee.jpg")), filename: "seed_coffee.jpg", content_type: 'image/jpg')
-  when 4
-    event.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_bbq.mp4")), filename: "seed_bbq.mp4", content_type: 'video/mp4')
-    event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_bbq_thombnail.jpg")), filename: "seed_bbq_thombnail.jpg", content_type: 'image/jpg')
-  when 5
-    event.thumbnail.attach(io: File.open(Rails.root.join('app', 'assets', 'images', "seed_game.jpg")), filename: "seed_game.jpg", content_type: 'image/jpg')
-  end
-
-  s += 1
 end
 
 # オンラインイベント20件
@@ -230,16 +244,18 @@ end
 
 
 # 卒業発表で使うデータ
-pre_user = User.create!(
-  name: "user_x@example.com",
-  email: "user_x@example.com",
-  password: "user_x@example.com",
-  address: "千葉県千葉市中央区新宿２－１",
-  introduction: "Hello! This is default introduction"
-)
+if Rails.env == "production"
+  pre_user = User.create!(
+    name: "user_x@example.com",
+    email: "user_x@example.com",
+    password: "user_x@example.com",
+    address: "千葉県千葉市中央区新宿２－１",
+    introduction: "Hello! This is default introduction"
+  )
 
-pre_user.own_groups.create!(
-  name: "group_x",
-  detail: "Hello! This is default detail",
-  permission: true
-)
+  pre_user.own_groups.create!(
+    name: "Japanese-English-Mandarine交流グループ",
+    detail: "Hello! This is default detail",
+    permission: true
+  )
+end

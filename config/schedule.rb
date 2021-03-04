e015225269b6984c164f005a8dd995ad6ebda7c4
kanami miyakawa <kedamap@yahoo.co.jp>
2021-03-04 09:40:19 +0000

    Test: whenever test

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,8 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
+set :output, 'log/crontab.log'
+set :environment, ENV['RAILS_ENV']
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -16,5 +17,7 @@
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
-
+every 1.minute do
+  rake "whenever_test:test"
+end
 # Learn more: http://github.com/javan/whenever
